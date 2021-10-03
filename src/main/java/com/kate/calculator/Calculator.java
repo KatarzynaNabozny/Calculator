f29473ca6f61c52721b4d9b3f7748182e9a1e812
Katarzyna Nabożny <knnabozny@gmail.com>
2021-10-03 17:31:43 +0200

    Add methods to Calculator

---
+++ src/main/java/com/kate/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.kate.calculator;
+
+public class Calculator {
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double substraction(double a, double b) {
+        return a - b;
+    }
+}
--- src/main/java/com/kate/calculator/CalculatorApplication.java
+++ src/main/java/com/kate/calculator/CalculatorApplication.java
@@ -8,6 +8,8 @@ public class CalculatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApplication.class, args);
+        Calculator calculator = new Calculator();
+        System.out.println("Add= " + calculator.add(1, 2));
+        System.out.println("Substraction= " + calculator.substraction(1, 2));
     }
-
 }
